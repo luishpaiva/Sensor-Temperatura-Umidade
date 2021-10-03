368243989338c669b0b59971676dda37c9afa483
Luis Henrique Paiva <luishp@hotmail.com.br>
2021-10-03 16:46:56 -0300

    Criada biblioteca em arquivo separado. Melhorada a lógica do laço de repetição.

--- main.py
+++ main.py
@@ -1,61 +1,80 @@
-# Importações
+# Importação da biblioteca 'wifi_lib.py'
+from wifi_lib import conecta
+
+# Importação de bibliotecas externas
 import dht
 import machine
+import urequests
 import time
 
-# Sensor e relé
-d = dht.DHT11(machine.Pin(4))         #d = DHT11
-r = machine.Pin(2, machine.Pin.OUT)   #r = relé
+# Receber nome da rede e senha para realizar a conexão
+ssid = input("Informe o nome da rede a qual deseja se conectar: ")
+senha = input("Informe a senha da rede: ")
 
-# Inicialização de variáveis
-r.value(0)
-temperatura_atual = 0
-humidade_atual = 0
+# Imprimir mensagem 'conectando'
+print("Conectando...")
 
-# Laço de repetição principal
-while True:
-    # Mensuração da temperatura e humidade
-    d.measure()
+# Nova instância de 'conecta' com o nome da rede e a senha como parâmetros
+station = conecta(ssid, senha)
+
+# Verifica se está conectado
+if not station.isconnected():
+    print("Não conectado! Verifique os dados de rede e a conexão e tente novamente.")
+else:
+    print("Conectado!")
     
-    # Recebimento dos dados  da mensuração da temperatura e humidade para poder imprimir e fazer as condições
-    temperatura_anterior = temperatura_atual
-    humidade_anterior = humidade_atual
-    temperatura_atual = d.temperature()
-    humidade_atual = d.humidity()
+    # Número da chave API do Thingspeak
+    api_key = input("Informe a chave API (API Key) do Thingspeak: ")
 
-    print("Temperatura = {}°C       Umidade = {}%".format(temperatura_atual, humidade_atual))
+    # Inicialização das variáveis do sensor DHT11 e do relé
+    d = dht.DHT11(machine.Pin(4))         #d = DHT11
+    r = machine.Pin(2, machine.Pin.OUT)   #r = relé
 
-    # Condições para ligar o relé
-    if temperatura_atual > 31 and temperatura_anterior <= 31:
-        print("======================================")
-        print("Temperatura maior que 31°C!")
-        if r.value() == 0 and humidade_atual <= 70:
-            print("Ligando o relé...")
-            r.value(1)
-        print("======================================")
-    if humidade_atual > 70 and humidade_anterior <= 70:
-        print("======================================")
-        print("Humidade relativa do ar maior que 70%!")
-        if r.value() == 0 and temperatura_atual <= 31:
-            print("Ligando o relé...")
-            r.value(1)
-        print("======================================")
+    # Inicialização do status do relé como desligado
+    r.value(0)
+
+    # Receber quantidade de vezes a ser repetida a leitura dos dados
+    quantidade = int(input("Informe a quantidade de leituras que deseja fazer (5760 = 1 dia): "))
     
-    # Condições para desligar o relé
-    if temperatura_atual <= 31 and temperatura_anterior > 31:
-        print("======================================")
-        print("Temperatura menor que 32°C!")
-        if r.value() == 1 and humidade_atual <= 70:
+    # Imprimir mensagem de início de leitura dos dados
+    print("Começando a leitura dos dados...")
+    
+    # Cabeçalho
+    print("\n===========================================")
+    print("        Início da leitura dos dados")
+    print("===========================================")
+
+    for i in range(quantidade):
+        # Mensuração da temperatura e umidade
+        d.measure()
+        
+        # Condição para ligar o relé
+        if (d.temperature() > 31 or d.humidity() > 70) and r.value() == 0:
+            print("-------------------------------------------")
+            print("Ligando o relé...")
+            print("-------------------------------------------")
+            r.value(1)
+
+        # Condição para desligar o relé
+        if (d.temperature() <= 31 and d.humidity() <= 70) and r.value() == 1:
+            print("-------------------------------------------")
             print("Desligando o relé...")
+            print("-------------------------------------------")
             r.value(0)
-        print("======================================")
-    if humidade_atual <= 70 and humidade_anterior > 71:
-        print("======================================")
-        print("Humidade relativa do ar menor que 71%!")
-        if r.value() == 1 and temperatura_atual <= 31:
-            print("Desligando o relé")
-            r.value(0)
-        print("======================================")
 
-    # Tempo de espera entre as medições
-    time.sleep(5)
+        # Imprimir valores da temperatura e umidade
+        print("{}.".format(i + 1), (" " * (len(str(quantidade)) - len(str(i + 1)))), "Temperatura = {}°C    Umidade = {}%".format(d.temperature(), d.humidity()))
+
+        # Upload dos dados de temperatura, umidade e status do relé no servidor Thingspeak
+        resposta = urequests.get("https://api.thingspeak.com/update?api_key={}&field1={}&field2={}&field3={}".format(api_key, d.temperature(), d.humidity(), r.value()))
+
+        # Fechamento da variável
+        resposta.close()
+
+        # Verificar para não precisar esperar 15 segundos no último laço
+        if (i < quantidade - 1):
+            # Tempo de espera para poder fazer novo upload no Thingspeak
+            time.sleep(15)
+
+    # Fechamento da conexão
+    station.disconnect()
